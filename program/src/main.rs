6e97ce31b46725b871245684e6f0e25e8a2e2f35
jongwhan lee <leejw51@gmail.com>
2019-06-03 20:14:41 +0900

    user control

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,11 +1,11 @@
 extern crate  smallnetwork;
 extern crate smallgameserver;
 use smallnetwork::Network::{Network};
-use smallgameserver::GameServer::{GameServer};
+use smallgameserver::GameServer::{GameServer, UserControl};
 fn main() {
-    let mut m = GameServer::new();
-    m.initialize();
-    //m.menu();
-    m.runServer();
+    let mut server = GameServer::new();
+    server.initialize();
+    //server.menu();
+    server.startServer();
 
 }
--- smallgameserver/src/GameServer.rs
+++ smallgameserver/src/GameServer.rs
@@ -4,15 +4,25 @@ use std::io;
 pub struct GameServer {
     version: String,
 }
+pub trait UserControl  {
+    fn initialize(&mut self);
+    fn startServer(&mut self) ;
+}
+//---------------------------------------------------------------------------------------------------
 impl GameServer {
     pub fn new() -> Self {
         GameServer {
             version: "1.0.0".to_string(),
         }
     }
-    pub fn initialize(&mut self) {
+    pub fn test(&mut self) 
+    {
+        println!("test");
+    }
+    pub fn setup(&mut self) {
       //  let info = myinfo::Information {info:"1.0.0"};
-        println!("game server version={}", self.version)
+        println!("game server version={}", self.version);
+        self.test();
     }
     pub fn runServer(&mut self) {
         println!("server");
@@ -42,3 +52,16 @@ impl GameServer {
 
 }
 
+impl UserControl for GameServer 
+{
+    fn initialize(&mut self) 
+    {
+        println!("UserControl initialize");
+        self.setup();
+    }
+     fn startServer(&mut self) 
+    {
+        println!("UserControl StartServer");
+        self.runServer();
+    }
+}
